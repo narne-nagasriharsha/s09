9ccde700eaa970813262405f611510d997839448
narne-nagasriharsha <narnenagasriharsha@gmail.com>
2025-07-23 09:54:30 +0530

    15

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S109">
+    <Router basename="/s09">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
